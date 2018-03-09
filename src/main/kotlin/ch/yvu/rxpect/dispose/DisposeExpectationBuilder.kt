3caa608be5e8354e3b95d05d7d4f6ad6c83a7641
Yves Bonjour <yves.bonjour@ergon.ch>
2018-03-09 15:57:50 +0100

    Adds DisposeExpectationBuilder for Observables

--- src/main/kotlin/ch/yvu/rxpect/dispose/DisposeExpectationBuilder.kt
+++ src/main/kotlin/ch/yvu/rxpect/dispose/DisposeExpectationBuilder.kt
@@ -4,6 +4,7 @@ import ch.yvu.rxpect.Expectation
 import ch.yvu.rxpect.ExpectationWithLatch
 import com.nhaarman.mockitokotlin2.whenever
 import io.reactivex.Maybe
+import io.reactivex.Observable
 import io.reactivex.Single
 import java.util.concurrent.CountDownLatch
 
@@ -19,6 +20,14 @@ fun <T> expectDispose(methodCall: Maybe<T>?): Expectation {
     val latch = CountDownLatch(1)
     whenever(methodCall).thenAnswer {
         Maybe.never<T>().doOnDispose { latch.countDown() }
+    }
+    return ExpectationWithLatch(latch)
+}
+
+fun <T> expectDispose(methodCall: Observable<T>?): Expectation {
+    val latch = CountDownLatch(1)
+    whenever(methodCall).thenAnswer {
+        Observable.never<T>().doOnDispose { latch.countDown() }
     }
     return ExpectationWithLatch(latch)
 }
---
+++ src/test/kotlin/ch/yvu/rxpect/dispose/DisposeObservableExpectationBuilderTest.kt
@@ -0,0 +1,33 @@
+package ch.yvu.rxpect.dispose
+
+import com.nhaarman.mockitokotlin2.mock
+import io.reactivex.Observable
+import org.junit.Test
+import org.mockito.exceptions.verification.WantedButNotInvoked
+
+class DisposeObservableExpectationBuilderTest {
+    @Test
+    fun buildsCorrectExpectationForSingleDisposed() {
+        val mock: TestClass = mock()
+        val expectation = expectDispose(mock.method())
+
+        val disposable = mock.method().subscribe()
+        disposable.dispose()
+
+        expectation.verify()
+    }
+
+    @Test(expected = WantedButNotInvoked::class)
+    fun buildsCorrectExpectationForSingleNotDisposed() {
+        val mock: TestClass = mock()
+        val expectation = expectDispose(mock.method())
+
+        mock.method().subscribe()
+
+        expectation.verify()
+    }
+
+    interface TestClass {
+        fun method(): Observable<Unit>
+    }
+}
