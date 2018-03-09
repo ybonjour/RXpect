576e601bdc0be6c1d0247defe1d3391d68f50269
Yves Bonjour <yves.bonjour@ergon.ch>
2018-03-09 20:36:39 +0100

    Adds DisposeExpectation

---
+++ src/main/kotlin/ch/yvu/rxpect/dispose/DisposeExpectation.kt
@@ -0,0 +1,12 @@
+package ch.yvu.rxpect.dispose
+
+import ch.yvu.rxpect.BaseExpectation
+import org.mockito.MockingDetails
+import org.mockito.exceptions.base.MockitoAssertionError
+import org.mockito.exceptions.verification.WantedButNotInvoked
+import org.mockito.invocation.Invocation
+
+class DisposeExpectation : BaseExpectation() {
+    override fun buildAssertionError(invocation: Invocation, mockingDetails: MockingDetails): MockitoAssertionError =
+        WantedButNotInvoked("The ${invocation.method.returnType} returned by ${invocation.method.name} has never been disposed")
+}
