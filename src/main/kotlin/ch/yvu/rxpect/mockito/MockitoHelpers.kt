8fcc4edb8d2f74c8f481c6c7fec3142f20e6c899
Yves Bonjour <yves.bonjour@ergon.ch>
2018-03-10 11:13:21 +0100

    Extracts getLastStubbing from extractLastInvocation

--- src/main/kotlin/ch/yvu/rxpect/mockito/MockitoHelpers.kt
+++ src/main/kotlin/ch/yvu/rxpect/mockito/MockitoHelpers.kt
@@ -7,10 +7,13 @@ import org.mockito.stubbing.Stubbing
 import java.util.TreeSet
 
 object MockitoHelpers {
-    fun <T> extractLastInvocation(ongoingStubbing: OngoingStubbing<T>): Invocation {
+    fun <T> extractLastInvocation(ongoingStubbing: OngoingStubbing<T>): Invocation =
+        getLastStubbing(ongoingStubbing.getMock()).invocation
+
+    private fun getLastStubbing(mock: Any): Stubbing {
         // We use the internal information, that Mockito tracks stubbings ordered in a TreeSet
         // so that we can extract the last (just added stubbing). This may break on Mockito updates
-        val stubbings = Mockito.mockingDetails(ongoingStubbing.getMock()).stubbings as TreeSet<Stubbing>
-        return stubbings.last().invocation
+        val stubbings = Mockito.mockingDetails(mock).stubbings as TreeSet<Stubbing>
+        return stubbings.last()
     }
 }
